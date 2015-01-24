b1aad3fb802a443d3fdd1d46910c2cdfc9cd1fa4
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2015-01-24 17:21:04 +0200

    Updated the version number.

--- satip.c
+++ satip.c
@@ -27,7 +27,7 @@
 #define GITVERSION ""
 #endif
 
-       const char VERSION[]     = "1.0.2" GITVERSION;
+       const char VERSION[]     = "2.0.0" GITVERSION;
 static const char DESCRIPTION[] = trNOOP("SAT>IP Devices");
 
 class cPluginSatip : public cPlugin {
