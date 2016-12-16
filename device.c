9c1dc3d6ac0c0ba66b44ad0311e7b7e8b8361ac3
REELcoder <beat.zahnd@gmail.com>
2016-12-16 08:45:35 +0100

    Temporary fix for OctopusNet

--- device.c
+++ device.c
@@ -417,6 +417,7 @@ bool cSatipDevice::OpenDvr(void)
   if (pTunerM)
      pTunerM->Open();
   isOpenDvrM = true;
+  fixedPidsM.Clear();
   return true;
 }
 
@@ -558,11 +559,11 @@ bool cSatipDevice::GetTSPacket(uchar *&dataP)
      else
         dataP = GetData();
 
-     if (dataP && (dataP[4] == 0x00) && (dataP[5] == 0x02) && ((dataP[6] & 0xf0) == 0xb0)) {
+     if (dataP && (dataP[4] == 0x00) && (dataP[5] == 0x02) && ((dataP[6] & 0xfc) == 0xb0)) {
         int pidA = ((dataP[1] & 0x1f) << 8) | dataP[2];
 	int pidB = GetPmtPid();
-        debug12("%s PID %d/%d 0x%02x%02x%02x%02x%02x%02x%02x", __PRETTY_FUNCTION__,
-			pidA, pidB, dataP[0], dataP[1], dataP[2], dataP[3], dataP[4], dataP[5], dataP[6]);
+        debug12("%s PID %d/%d 0x%02x%02x%02x%02x%02x%02x%02x %d", __PRETTY_FUNCTION__,
+			pidA, pidB, dataP[0], dataP[1], dataP[2], dataP[3], dataP[4], dataP[5], dataP[6], fixedPidsM.IndexOf(pidA));
      }
 
      return true;
