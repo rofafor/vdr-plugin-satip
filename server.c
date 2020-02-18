17c36fa9d2575478d3a62c65008ab544e640ffbb
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2020-02-18 17:55:22 +0200

    Added quirks for Inverto IDL-400s.

--- server.c
+++ server.c
@@ -119,6 +119,7 @@ cSatipServer::cSatipServer(const char *srcAddressP, const char *addressP, const 
      // Elgato EyeTV Netstream 4Sat ?
      if (strstr(*descriptionM, "GSSBOX") ||                    // Grundig Sat Systems GSS.box DSI 400
          strstr(*descriptionM, "DIGIBIT") ||                   // Telestar Digibit R1
+         strstr(*descriptionM, "Multibox-030227:SAT>IP") ||    // Inverto IDL-400s
          strstr(*descriptionM, "Triax SatIP Converter")        // Triax TSS 400
         )
         quirkM |= eSatipQuirkSessionId;
@@ -150,6 +151,7 @@ cSatipServer::cSatipServer(const char *srcAddressP, const char *addressP, const 
      // These devices don't support auto-detection of pilot tones
      if (strstr(*descriptionM, "GSSBOX") ||                    // Grundig Sat Systems GSS.box DSI 400
          strstr(*descriptionM, "DIGIBIT") ||                   // Telestar Digibit R1
+         strstr(*descriptionM, "Multibox-030227:SAT>IP") ||    // Inverto IDL-400s
          strstr(*descriptionM, "Triax SatIP Converter") ||     // Triax TSS 400
          strstr(*descriptionM, "KATHREIN SatIP Server")        // Kathrein ExIP 414/E
         )
